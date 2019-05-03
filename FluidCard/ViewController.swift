d4ef7a4440daf9bb2ebf33f326fb68755a03fb4d
askopin <askopin@gmail.com>
2019-05-03 22:22:45 +0300

    init project

---
+++ FluidCard/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FluidCard
+//
+//  Created by Anton Skopin on 03/05/2019.
+//  Copyright © 2019 Cuberto. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
